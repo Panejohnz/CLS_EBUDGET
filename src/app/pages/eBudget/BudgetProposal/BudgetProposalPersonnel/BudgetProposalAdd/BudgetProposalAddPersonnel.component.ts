ce42e82c4c7b8fe06e3e6f61924207f0013c38c7
Pitchakorn.p <pitchakorn.p@fsa.co.th>
2026-05-12 18:00:02 +0700

    m

--- src/app/pages/eBudget/BudgetProposal/BudgetProposalPersonnel/BudgetProposalAdd/BudgetProposalAddPersonnel.component.ts
+++ src/app/pages/eBudget/BudgetProposal/BudgetProposalPersonnel/BudgetProposalAdd/BudgetProposalAddPersonnel.component.ts
@@ -92,7 +92,7 @@ export class ProjectBudgetProposalAddPersonnelComponent {
     });
 
     this.Get_Dropdown_list()
-        console.log('เข้า model', this.model);
+    console.log('เข้า model', this.model);
   }
   ngOnChanges() {
 
@@ -242,8 +242,8 @@ export class ProjectBudgetProposalAddPersonnelComponent {
         this.plan.subUnitActivity = item.Fk_Budget_Type_Id;
         this.formTitle = item.Expense_Name
 
-        console.log('หาย',this.model);
-        
+        console.log('หาย', this.model);
+
       })
 
   }
@@ -419,10 +419,50 @@ export class ProjectBudgetProposalAddPersonnelComponent {
         this.model.Budget_Request.Proposer_Position
 
     };
-    console.log('1', this.model.Budget_Request_Detail_Item)
+
+    const getId = (obj: any, key: string) =>
+      typeof obj === 'object' ? obj?.[key] : obj;
+    const payload_plan = {
+      BgYear: "2569",
+      Project_Id: this.model.Project_Id,
+
+      Department_Id: getId(this.model.selectedDepartment, 'Department_Id'),
+      Department_Name: this.model.selectedDepartment?.Department_Name,
+
+      Fk_Plan_Id: getId(this.model.selectedPlan, 'Plan_Id'),
+      Plan_Name: this.model.selectedPlan?.Plan_Name,
+
+      Fk_Expense_List: getId(this.model.projectType, 'Expense_Id'),
+      Expense_List: this.model.projectType?.Expense_Name,
+
+      Fk_Product_Id: getId(this.model.selectedProduct, 'Product_Id'),
+      Product_Name: this.model.selectedProduct?.Product_Name,
+
+      Fk_Activity_Id: getId(this.model.selectedActivity, 'Activity_Id'),
+      Activity_Name: this.model.selectedActivity?.Activity_Name,
+
+      Fk_Budget_Type: getId(this.model.selectedBudget, 'Budget_Type_Id'),
+      Budget_Type: this.model.selectedBudget?.Budget_Type_Name,
+
+      Project_Name: this.model.Project_Name,
+      Used_BG: this.model.Used_BG,
+      Project_Type_Id: this.model.Project_Type_Id,
+
+      Project_Year_Count: this.model.Project_Year_Count,
+      Project_Year_Number: this.model.Project_Year_Number,
+
+      Operation1: this.model.Operation1,
+      Operation2: this.model.Operation2,
+      Proposer_Name: this.model.Proposer_Name,
+      Proposer_Position: this.model.Proposer_Position,
+    };
+
+    this.model.Project_Plan_Detail = this.mapActivities();
 
 
-
+    // if (!this.validateBeforeSave(this.model.activities)) {
+    //   return;
+    // }
     const userConfirmed = await confirmAlert('info', 'ต้องการบันทึกข้อมูล ?', '');
 
     if (userConfirmed) {
@@ -430,28 +470,28 @@ export class ProjectBudgetProposalAddPersonnelComponent {
         FUNC_CODE: this.model.Budget_Request.Request_Id > 0
           ? "FUNC-Update_Budget_Request"
           : "FUNC-Insert_Budget_Request",
-
+        Project_Plan: payload_plan,
         Budget_Request: payload,
-        Budget_Request_Detail_Item: this.model.Budget_Request_Detail_Item
-        // Project_Detail: {
-        //   ...this.model.Project_Detail,
+        Budget_Request_Detail_Item: this.model.Budget_Request_Detail_Item,
+        Project_Detail: {
+          ...this.model.Project_Detail,
 
-        //   Start_Date: this.toDotNetDate(this.model.Project_Detail.Start_Date),
-        //   End_Date: this.toDotNetDate(this.model.Project_Detail.End_Date)
-        // },
-        // Project_Objective: this.model.Project_Objective,
-        // Project_Plan_Detail: this.model.Project_Plan_Detail,
-        // Project_Plan_Level1: this.model.Project_Plan_Level1,
-        // Project_Plan_Level2: this.model.Project_Plan_Level2,
-        // Project_Plan_Level1_Sub: this.model.Project_Plan_Level1_Sub,
-        // Project_Outcome: this.model.Project_Outcome,
-        // Project_Output: this.model.Project_Output,
-        // Project_Plan_Level3: this.model.Project_Plan_Level3,
-        // Project_Coordinator: this.model.Project_Coordinator,
-        // Project_Cabinet: this.model.Project_Cabinet,
-        // Project_Security: this.model.Project_Security,
-        // Project_Expected: this.model.Project_Expected,
-        // Project_TargetGroup: this.model.Project_TargetGroup,
+          Start_Date: this.toDotNetDate(this.model.Project_Detail.Start_Date),
+          End_Date: this.toDotNetDate(this.model.Project_Detail.End_Date)
+        },
+        Project_Objective: this.model.Project_Objective,
+        Project_Plan_Detail: this.model.Project_Plan_Detail,
+        Project_Plan_Level1: this.model.Project_Plan_Level1,
+        Project_Plan_Level2: this.model.Project_Plan_Level2,
+        Project_Plan_Level1_Sub: this.model.Project_Plan_Level1_Sub,
+        Project_Outcome: this.model.Project_Outcome,
+        Project_Output: this.model.Project_Output,
+        Project_Plan_Level3: this.model.Project_Plan_Level3,
+        Project_Coordinator: this.model.Project_Coordinator,
+        Project_Cabinet: this.model.Project_Cabinet,
+        Project_Security: this.model.Project_Security,
+        Project_Expected: this.model.Project_Expected,
+        Project_TargetGroup: this.model.Project_TargetGroup,
       };
 
       this.serviceebud.GatewayGetData(model).subscribe(() => {
@@ -462,6 +502,112 @@ export class ProjectBudgetProposalAddPersonnelComponent {
 
     }
 
+  }
+  mapActivities() {
+
+    const activities = this.model.activities || [];
+
+    return activities.map((act: any, i: number) => ({
+
+      Project_Detail_Id: act.id,
+      Activity_Name: act.name,
+      Responsible: act.owner,
+      Seq: act.Seq ?? (i + 1),
+
+      Used_BG: act.noBudget ? 0 : 1,
+      Is_Consult: act.consult ? 1 : 0,
+
+      Months: act.quarters.flatMap((q: any) => q.months),
+
+      OtherExpenses: act.otherExpenses || [],
+
+      SubActivities: (act.SubActivities || []).map((sub: any, j: number) => ({
+
+        Project_Detail_Id: sub.Project_Detail_Id,
+        Activity_Name: sub.name,
+        Responsible: sub.owner,
+        Seq: sub.Seq ?? (j + 1),
+
+        Used_BG: sub.noBudget ? 0 : 1,
+        Is_Consult: sub.consult ? 1 : 0,
+
+        Months: sub.quarters.flatMap((q: any) => q.months),
+
+        OtherExpenses: sub.otherExpenses || []
+
+      }))
+
+    }));
+  }
+  validateBeforeSave(activities: any[]): boolean {
+
+    if (this.model.Project_Id) {
+      return true;
+    }
+    for (let i = 0; i < activities.length; i++) {
+
+      const act = activities[i];
+
+      const m = Number(this.calcMultiplierTotalFromModel(act).toFixed(2));
+      const p = Number(this.getActivityTotal(act).toFixed(2));
+
+      if (m !== p) {
+        basicAlert(
+          'warning',
+          `กิจกรรมที่ ${i + 1} ยอดไม่เท่ากัน`,
+          `ผลคูณ = ${m.toLocaleString()} | วางแผน = ${p.toLocaleString()}`
+        );
+        return false;
+      }
+    }
+
+    return true;
+  }
+  calcMultiplierTotalFromModel(act: any): number {
+
+    // 🔥 มี sub → รวม sub
+    if (act.SubActivities?.length) {
+      return act.SubActivities.reduce((sum: number, sub: any) => {
+        return sum + this.calcMultiplierTotalFromModel(sub);
+      }, 0);
+    }
+
+    // 🔥 คำนวณจาก otherExpenses ตรงๆ
+    return (act.otherExpenses || []).reduce((sum: number, item: any) => {
+      return sum + (item.total || item.Total || 0);
+    }, 0);
+  }
+  getActivityTotal(act: any): number {
+
+    let total = 0;
+
+    // รวมกิจกรรมหลัก
+    act.quarters.forEach((q: any) => {
+      q.months.forEach((m: any) => {
+        total += Number(m.budget) || 0;
+      });
+    });
+
+    // รวมกิจกรรมย่อย
+    total += this.getSubActivitiesTotal(act);
+
+    return total;
+  }
+  getSubActivitiesTotal(act: any): number {
+
+    let total = 0;
+
+    (act.SubActivities || []).forEach((sub: any) => {
+
+      sub.quarters?.forEach((q: any) => {
+        q.months?.forEach((m: any) => {
+          total += Number(m.budget) || 0;
+        });
+      });
+
+    });
+
+    return total;
   }
   toDotNetDate(dateStr: string): string | null {
     if (!dateStr) return null;
